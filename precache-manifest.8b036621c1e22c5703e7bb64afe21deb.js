5be01fc7b1c86790c2cbe86feee9ef76f9d5eaf5
Anurag <myanuragojha@gmail.com>
2019-08-20 15:17:03 +0530

    Updates

---
+++ precache-manifest.8b036621c1e22c5703e7bb64afe21deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c134195ff5427f75ca3ad179ce94afc6",
+    "url": "/basic-react/index.html"
+  },
+  {
+    "revision": "198259746c248ab283a5",
+    "url": "/basic-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d4767a828107541b5bd5",
+    "url": "/basic-react/static/js/2.69966d15.chunk.js"
+  },
+  {
+    "revision": "198259746c248ab283a5",
+    "url": "/basic-react/static/js/main.481dd6bd.chunk.js"
+  },
+  {
+    "revision": "0019ab418db57578900c",
+    "url": "/basic-react/static/js/runtime~main.73bb06a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basic-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
