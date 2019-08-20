7aefc2ef0c68e658314a30186f25f131173bc2c6
Anurag <myanuragojha@gmail.com>
2019-08-20 15:45:25 +0530

    added dummy page

--- basic-react/src/App.js
+++ basic-react/src/App.js
@@ -1,27 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Dummy from "./Dummy";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <Router basename="basic-react/">
+      <Route path="/" exact component={Dummy} />
     </Router>
   );
 }
